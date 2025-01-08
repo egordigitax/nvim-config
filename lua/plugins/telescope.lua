9f2f2a57fd841e73ac8a8e9c26c6ac2dd52694c1
egordigitax <fantomaka@gmail.com>
2025-01-08 06:42:22 +0500

    fix live_grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,24 @@
 return {
-    'nvim-telescope/telescope.nvim',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      opts={
-        pickers = {
-        colorscheme = {
-          enable_preview = true
-          }
-        }
-      },
-  }
+  'nvim-telescope/telescope.nvim',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  opts = {
+    defaults = {
+      vimgrep_arguments = {
+        'rg',
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '--smart-case',
+        '--max-filesize', '1M',
+        '--ignore-file', '.gitignore',  -- Use existing .gitignore rules if needed
+      }
+    },
+    pickers = {
+      colorscheme = {
+        enable_preview = true
+      }
+    }
+  },
+}
